4b1b9bc66d94056927477bfae34dc0a617df904c
bishalacharya6 <bishalacharya673@gmail.com>
2024-05-04 23:25:41 +0545

    asdf

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -50,7 +50,7 @@ function Footer() {
             </div>
 
             {/* Information Section */}
-            <div className="grid grid-cols-1 gap-8 sm:gap-6 mb-4">
+            <div className="grid grid-cols-2 gap-8 sm:gap-6 mb-4">
               <div>
                 <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Products</h2>
                 <ul className="text-gray-500 font-medium">
